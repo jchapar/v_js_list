69365d52d9090f7e92d7a5d00028a5d7393bab61
jchapar <chaparrojcjr@gmail.com>
2023-05-09 18:58:08 -0700

    first initial commit

---
+++ 00-starter-files/shopping-list/script.js
@@ -0,0 +1,45 @@
+const itemForm = document.querySelector('#item-form')
+const itemInput = document.querySelector('#item-input')
+const itemList = document.querySelector('#item-list')
+
+function addItem(e) {
+  e.preventDefault()
+
+  const newItem = itemInput.value
+
+  // Validate input
+  if (newItem === '') {
+    alert('Please add an item')
+    return
+  }
+
+  // Create List item
+  const li = document.createElement('li')
+  li.appendChild(document.createTextNode(newItem))
+
+  const button = createButton('remove-item btn-link text-red')
+  li.appendChild(button)
+
+  itemList.appendChild(li)
+
+  itemInput.value = ''
+}
+
+// Create Button
+function createButton(classes) {
+  const button = document.createElement('button')
+  button.className = classes
+  const icon = createIcon('fa-solid fa-xmark')
+  button.appendChild(icon)
+  return button
+}
+
+// Create button icon
+function createIcon(classes) {
+  const icon = document.createElement('i')
+  icon.className = classes
+  return icon
+}
+
+//////////////////////////// Event Listeners
+itemForm.addEventListener('submit', addItem)
